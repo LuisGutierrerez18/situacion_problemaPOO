807e8542f1abb22c9c3050af38f785e890f6d414
Luis Gutierrez <111073914+LuisGutierrerez18@users.noreply.github.com>
2023-07-25 16:57:42 -0600

    Add files via upload

---
+++ AeropuertoMain.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream> // prints
+#include <string>
+using namespace std;
+
+#include "Aeropuerto.h"
+int main(){
+
+    Aeropuerto aeropuerto1(20);
+
+    aeropuerto1.aeropuerto_toString(); 
+
+    return 0;
+}
