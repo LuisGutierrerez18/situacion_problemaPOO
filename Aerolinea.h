cfb9df6cf4e7372125e959e47d1265d8223f1b81
Luis Gutierrez <111073914+LuisGutierrerez18@users.noreply.github.com>
2023-07-29 10:23:46 -0600

    Update Aerolinea

--- Aerolinea.h
+++ Aerolinea.h
@@ -1,3 +1,8 @@
+#include<iostream> // prints
+#include <string>
+
+using namespace std;
+
 class Aerolinea{
 private:
     string nombre;
@@ -7,43 +12,31 @@ private:
     //flota Avion;
     //vuelo Itinerario;
 public:
-    Aerolinea();
-    Aerolinea(string,string,float,float);
+    
+    Aerolinea(string _nombre,string _origen,float _ingresos,float _comision){
+        nombre=_nombre;
+        origen=_origen;
+        ingresos=_ingresos;
+        comision=_comision;
+    }
     //getVuelo()
-    void calcula_comision();
-    void setIngresos();
-    void muestraAerolinea();
+    void calcula_comision(){
+        comision = comision*0.10;
+    }
+    void setIngresos(float _ingresos){
+        ingresos = _ingresos;
+    }
+    
+    void muestraAerolinea(){
+        cout<<endl<<"Informacion de la aerolinea"<<endl;
+        cout<<"Nombre: "<<nombre<<endl;
+        cout<<"Origen: "<<origen<<endl;
+        cout<<"Ingresos: "<<ingresos<<endl;
+        cout<<"Comision: "<<comision<<endl;
+        cout<<"Ingresos: "<<ingresos+comision;
+    }
+
 };
 
-Aerolinea::Aerolinea(){
-    //Avion _avion;
-    //Itinerario _vuelo;
 
-    nombre="Aeromexico";
-    origen="Mexicano";
-    ingresos=0.00;
-    comision=10000.00;
-    //flota=_avion;
-    //vuelo=_vuelo;
-}
 
-Aerolinea::Aerolinea(string _nombre,string _origen,float _ingresos,float _comision){
-    nombre=_nombre;
-    origen=_origen;
-    ingresos=_ingresos;
-    comision=_comision;
-    //flota=_flota;
-    //vuelo=_vuelo;
-}
-
-void Aerolinea::muestraAerolinea(){
-    cout<<endl<<"Informacion de la aerolinea"<<endl;
-    cout<<"Nombre: "<<nombre<<endl;
-    cout<<"Origen: "<<origen<<endl;
-    cout<<"Ingresos: "<<ingresos<<endl;
-    cout<<"Comision: "<<comision<<endl;
-    //cout<<"Flota: "<<endl;
-    //propietario.muestraAvion();
-    //cout<<"Vuelo: "<<endl;
-    //propietario.muestraItinerario();
-}
