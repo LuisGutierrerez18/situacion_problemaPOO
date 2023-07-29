4aec506b341e2993d04d16ea0fefb10b5d797094
melmramxs <138227014+melmramxs@users.noreply.github.com>
2023-07-29 07:15:15 -0700

    Aerolinea.h

--- Aerolinea.h
+++ Aerolinea.h
@@ -1,9 +1,49 @@
-#include<iostream> // prints
-#include <string>
-
-using namespace std;
-
 class Aerolinea{
-    private:
-
+private:
+    string nombre;
+    string origen;
+    float ingresos;
+    float comision;
+    //flota Avion;
+    //vuelo Itinerario;
+public:
+    Aerolinea();
+    Aerolinea(string,string,float,float);
+    //getVuelo()
+    void calcula_comision();
+    void setIngresos();
+    void muestraAerolinea();
 };
+
+Aerolinea::Aerolinea(){
+    //Avion _avion;
+    //Itinerario _vuelo;
+
+    nombre="Aeromexico";
+    origen="Mexicano";
+    ingresos=0.00;
+    comision=10000.00;
+    //flota=_avion;
+    //vuelo=_vuelo;
+}
+
+Aerolinea::Aerolinea(string _nombre,string _origen,float _ingresos,float _comision){
+    nombre=_nombre;
+    origen=_origen;
+    ingresos=_ingresos;
+    comision=_comision;
+    //flota=_flota;
+    //vuelo=_vuelo;
+}
+
+void Aerolinea::muestraAerolinea(){
+    cout<<endl<<"Informacion de la aerolinea"<<endl;
+    cout<<"Nombre: "<<nombre<<endl;
+    cout<<"Origen: "<<origen<<endl;
+    cout<<"Ingresos: "<<ingresos<<endl;
+    cout<<"Comision: "<<comision<<endl;
+    //cout<<"Flota: "<<endl;
+    //propietario.muestraAvion();
+    //cout<<"Vuelo: "<<endl;
+    //propietario.muestraItinerario();
+}
